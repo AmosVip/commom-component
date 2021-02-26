3e5c4d50b3c36a1103dea37da4f2c3df726bc9e3
amos <hui.li1@yintech.cn>
2021-02-26 18:27:06 +0800

    msg

---
+++ app/src/main/java/com/amos/study/javabase/JavaCollection.java
@@ -0,0 +1,51 @@
+package com.amos.study.javabase;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
+
+/**
+ * @author: amos
+ * @date: 2021/2/22 13:47
+ * @description: java 容器
+ */
+public class JavaCollection {
+
+    private void collection(){
+        //Collection<Integer> col = Collections
+    }
+
+    private void map(){
+
+    }
+
+    private void set(){
+        Set<Integer> temp = new HashSet<>();
+        Set<Integer> temp2 = new LinkedHashSet<>();
+
+    }
+
+    private void list(){
+        List<Integer> temp = new ArrayList<>();
+        List<Integer> temp2 = new LinkedList<>();
+        Vector<Integer> temp3 = new Vector<>();
+
+    }
+
+    /**
+     * 数组
+     */
+    private void array() {
+        int[] ary = new int[]{1, 2};
+        int[] test ={1,23,4};
+        int[] ss = new int[2];
+        ss[0] = 1;
+        ss[1] = 2;
+    }
+}
---
+++ app/src/main/java/com/amos/study/ui/FlycoTabLayoutActivity.kt
@@ -0,0 +1,16 @@
+package com.amos.study.ui
+
+import android.os.Bundle
+import com.amos.study.BaseActivity
+
+/**
+ * @author: amos
+ * @date: 2021/2/24 11:26
+ * @description:
+ */
+class FlycoTabLayoutActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
